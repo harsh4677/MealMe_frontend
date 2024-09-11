96b2bf9a14fa2d9c46a6ebff10834a57ad06aa96
harsh4677 <harshsantoshsalve@gmail.com>
2024-09-11 07:58:32 +0530

    Done with frontend

--- src/auth/ProtectedRoute.tsx
+++ src/auth/ProtectedRoute.tsx
@@ -1,17 +1,18 @@
-import { useAuth0 } from "@auth0/auth0-react"
+import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate, Outlet } from "react-router-dom";
 
-const ProtectedRoutes = ()=>{
-    const {isAuthenticated, isLoading} = useAuth0();
+const ProtectedRoute = () => {
+  const { isAuthenticated, isLoading } = useAuth0();
 
-    if(isLoading){
-        return null;
-    }
-    if(isAuthenticated){
-        return <Outlet/>
-    }
+  if (isLoading) {
+    return null;
+  }
 
-    return <Navigate to="/" replace/>
-}
+  if (isAuthenticated) {
+    return <Outlet />;
+  }
 
-export default ProtectedRoutes;
+  return <Navigate to="/" replace />;
+};
+
+export default ProtectedRoute;
